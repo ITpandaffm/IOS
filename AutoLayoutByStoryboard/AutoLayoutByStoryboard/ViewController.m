df0787c461b4393abae57d1bd6524876999b36c4
ffm <fengfuming2007@126.com>
2016-08-17 07:26:25 +0800

    AnutoLayoutByStoryboard
    this VERSION achieve auto layout by storyboard

---
+++ AutoLayoutByStoryboard/AutoLayoutByStoryboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayoutByStoryboard
+//
+//  Created by ffm on 16/7/28.
+//  Copyright © 2016年 ITPanda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
