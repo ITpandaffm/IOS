3f274529e4d1f3082e1ce331e3b88ef260aa147c
ffm <fengfuming2007@126.com>
2016-09-29 10:19:34 +0800

    RevealAnimationFromAndroid
    it;s a animation in github form android which is fun. so translate it
    to iOS hope you like it

---
+++ RevealAnimation FromAndroid/RevealAnimation/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  RevealAnimation
+//
+//  Created by ffm on 16/9/6.
+//  Copyright © 2016年 ITPanda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+
+}
+
+
+@end
